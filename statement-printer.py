3a52c7f1d944a19a56278624d7e47842d9fe7d45
libream <willietrombon3@gmail.com>
2017-03-02 16:38:14 -0500

    Add files via upload

---
+++ statement-printer.py
@@ -0,0 +1,7 @@
+myit = 'y'
+mystring = ''
+
+while myit == 'y':
+    mystring = input('Please enter a statement to print.\n')
+    print(mystring)
+    myit = input('Would you like to do it again? y or n: ')
