0ccaa9c13ca1e24f36b13705d9a05bc4a0294095
thelegendarysquare <thelegendarysquare@gmail.com>
2017-03-02 21:45:27 -0500

    more functionality
    added an if statement that lets you choose to go again. else the program
    will exit the loop

--- statement-printer.py
+++ statement-printer.py
@@ -5,3 +5,9 @@ while myit == 'y':
     mystring = input('Please enter a statement to print.\n')
     print(mystring)
     myit = input('Would you like to do it again? y or n: ')
+    if myit == 'y':
+        mystring
+    else:
+        print('you have chosen not to do it again')
+        break
+
