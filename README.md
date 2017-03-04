40bf1e35b8f53bd4dcdcaab52cd3ded8f60c5119
JakeGPi <jgreenisen@gmail.com>
2017-03-03 22:22:01 -0500

    Added a number printer and changed read me.
    I added a number printer at the end of the statement printer. Are with
    that, I updated the "Read Me" file to reflect the change in code. Screen
    shots uploaded.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # statement-printer
 a statement printer
-A basic python program that takes a statement and prints it for the user.
+A basic python program that takes a statement and a number and prints it for the user.
--- statement-printer.py
+++ statement-printer.py
@@ -1,13 +1,23 @@
 myit = 'y'
 mystring = ''
+myIT ='y'
+myInt=0
 
 while myit == 'y':
-    mystring = input('Please enter a statement to print.\n')
+    mystring = input('Please enter a statement to print.')
     print(mystring)
-    myit = input('Would you like to do it again? y or n: ')
+    myit = input('Would you like to do it again? y or n: \n')
     if myit == 'y':
         mystring
     else:
+        print('you have chosen not to do it again\n')
+        break
+while myIT == 'y':
+    myInt = input('Please enter a number to print.')
+    print(myInt)
+    myIT = input('Would you like to do it again? y or n: \n')
+    if myIT == 'y':
+        myInt
+    else:
         print('you have chosen not to do it again')
         break
-
